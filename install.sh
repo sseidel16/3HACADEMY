8722f9e1e4ec8a75c0fdbe676a1e4fbf7f9df3ee
Stephen Seidel <stephen.a.seidel@gmail.com>
2021-08-15 19:50:43 -0700

    some weird perms that made things work

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+APP_DATA_DIR=/app
+
 # create and copy necessary data directories
-sudo mkdir /app
-sudo chmod +777 /app
-cp -rf ./app
+sudo mkdir ${APP_DATA_DIR}
+sudo chmod +777 ${APP_DATA_DIR}
+cp -rf ./app/* ${APP_DATA_DIR}
 
+# give permissions for everything
+sudo chmod -R +777 ${APP_DATA_DIR}
 
+# remove unwanted .gitkeep s
+find ${APP_DATA_DIR} -type f -name '.gitkeep' -delete
 
+sudo chmod 0644 ${APP_DATA_DIR}/onlyoffice/mysql/conf.d/onlyoffice.cnf
 
 
